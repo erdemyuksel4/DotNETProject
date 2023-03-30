3671bc168d9db74ed2e24b99851ecb1ec4a5851c
Erdem <Erdem@LAPTOP-5QVPUIEC>
2023-03-30 23:06:23 +0300

    Fouth commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,34 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductTest();
+            //CateogoryTest();
+        }
 
-            foreach (var product in productManager.GetAll())
+        private static void CateogoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetProductDetails();
+            if(result.Success)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName+"/"+product.CategoryName);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
